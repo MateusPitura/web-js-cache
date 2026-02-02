f9e1f89a61c5683549e7e1cf76c6b40911e57edc
MateusPitura <mateuspitura@gmail.com>
2026-02-01 23:00:08 -0300

    fix: update version text to 'Version 2' in index.html and script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     const paragraph = document.getElementById('paragraph');
-    paragraph.textContent = 'Version 1';
+    paragraph.textContent = 'Version 2';
 });
