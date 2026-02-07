1132f98123e1286abef620a951b3d50966e9b0b6
MateusPitura <mateuspitura@gmail.com>
2026-02-06 22:47:11 -0300

    Update asset paths in index.html to point to the example directory

---
+++ assets/Home-DkmrzBNb.js
@@ -0,0 +1,1 @@
+import{j as e,L as o}from"./index-Fpt7loZh.js";function r(){return e.jsxs("div",{children:[e.jsx("h1",{children:"Home"}),e.jsx(o,{to:"/somePage",children:"Go to SomePage"})]})}export{r as default};
--- assets/Home-vRNRWT2O.js
+++
@@ -1,1 +0,0 @@
-import{j as e,L as o}from"./index-DmhJXI2Q.js";function r(){return e.jsxs("div",{children:[e.jsx("h1",{children:"Home"}),e.jsx(o,{to:"/somePage",children:"Go to SomePage"})]})}export{r as default};
